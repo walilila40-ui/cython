46e4b78259736a088337816dc14951aa81db46d1
😂TERA ABU JAN🥵 <walilila40@gmail.com>
2026-09-23 23:28:44 +0300

    Update Khan.py

--- Khan.py
+++ Khan.py
@@ -1,7 +1,3 @@
 import RTTR
 
-if hasattr(RTTR, "menu"):
-    RTTR.menu()
-else:
-    print("ERROR: RTTR.menu() not found")
-    print("Available:", [x for x in dir(RTTR) if not x.startswith("_")])
+RTTR.main()
