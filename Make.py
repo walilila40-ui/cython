5f2c640a3d4fcad3124861ea68341291da95d42a
😂TERA ABU JAN🥵 <walilila40@gmail.com>
2026-09-23 22:20:59 +0300

    Add files via upload

---
+++ Make.py
@@ -0,0 +1,16 @@
+import sys
+
+if sys.version_info[:2] == (3, 11):
+    import SASA
+    SASA.main()
+
+elif sys.version_info[:2] == (3, 13):
+    import ASAS
+    ASAS.main()
+
+else:
+    raise RuntimeError(
+        f"Unsupported Python version: {sys.version_info.major}.{sys.version_info.minor}"
+    )
+
+
